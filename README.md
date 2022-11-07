7fffa28b1d954559771ccf6c41be1db47a3ad769
Paul van Schaik <paul@schaikmail.nl>
2022-11-07 16:04:07 +0100

    Test een push

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # installers
 Install my own system
+
+Testen of dit ook werkt.
---
+++ swap_keyboard kopie.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sleep 10
+
+xmodmap -e "keycode 94 = grave asciitilde"
+xmodmap -e "keycode 49 = section plusminus"
